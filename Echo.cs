4019807a2fb0e37fc1f78d619af788e5acd9b659
Nikita Anisimov <nikitanisimov98@gmail.com>
2024-05-26 14:03:30 +1000

    Окрашивавние

--- Echo.cs
+++ Echo.cs
@@ -23,6 +23,7 @@ class MainClass
         {
             modif = modif.Remove(0, 2);
         }
+       Console.BackgroundColor = (ConsoleColor)deep;
         Console.WriteLine("...{0}",modif);
 
         if (deep > 1)
